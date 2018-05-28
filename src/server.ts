ef9305ebdeca006bf2999959bd81d7850f4dd128
Glitch (snake-api) <none>
2018-05-28 12:42:19 +0000

    🏄🐣 Updated with Glitch

--- src/server.ts
+++ src/server.ts
@@ -10,11 +10,17 @@ console.log('Running.');
 
 const app = express();
 
+app.use((_, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(bodyParser.json());
 
 app.post(`/${VERSION}/scores`, scores.create);
 app.get(`/${VERSION}/scores`, scores.list);
-app.get('/', (_, res) => res.status(200).send('Try /v1/scores. ;)'));
+app.get('/', (_, res) => res.status(200).send("Try /v1/scores. ;)"));
 
 const listener = app.listen(process.env.PORT, () => {
   console.log(
